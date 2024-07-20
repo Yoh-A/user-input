64635f2f9a76a06e36afebb3cefd92e7c1aea4c9
Yoh-A <ekmuna20@gmail.com>
2024-07-20 14:21:50 +0300

    first commit

---
+++ user-input.py
@@ -0,0 +1,11 @@
+# ask user for the name and store it in a variable.
+name = ("What is your name?")
+
+# ask for the age and store in a variable.
+age = ("How old are you?" )
+
+# ask for the location and store it in a variable.
+location = ("Where do you live?")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+# we are debugging
